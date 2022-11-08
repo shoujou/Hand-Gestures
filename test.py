82699107eb0adb8803c24c9ac510cd7afa6b2fbe
shoujou <kohei.0925@icloud.com>
2022-11-08 13:42:23 +0900

    change test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import sys,cv2
 
+args = sys.argv
+path = args[1]
+
 # 対象画像読み込み
-img = cv2.imread("archive/test/test/1/901.jpg",cv2.IMREAD_COLOR)
+img = cv2.imread(path,cv2.IMREAD_COLOR)
 
 # 画像の大きさを取得
 height, width, channels = img.shape[:3]
 print("width: " + str(width))
 print("height: " + str(height))
+print("channels: " + str(channels))
